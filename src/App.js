0882eefa55c5a9aabb2e0a722f2d04c76e39a53c
Ale Soto <asoto@lanacion.com.ar>
2019-07-07 22:15:30 -0300

    agrego galeria

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 import ModalContainer from './pages/containers/modalContainer';
 import Modal from './pages/components/modal';
 
+
+
 class App extends Component {
+  
   constructor(props) {
     super(props);
 
@@ -16,9 +19,17 @@ class App extends Component {
     }
     this.state = {
       liked: like,
-      showModal: false
+      showModal: false,
+      imagesIndex: 0,
+      imagesSources: {
+          0: "https://imgar.zonapropcdn.com/avisos/1/00/44/82/57/21/360x266/1698490754.jpg",
+          1: "https://imgar.zonapropcdn.com/avisos/1/00/44/82/57/21/360x266/1698490759.jpg",
+          2: "https://imgar.zonapropcdn.com/avisos/1/00/44/82/57/21/360x266/1698490765.jpg",
+          3: "https://imgar.zonapropcdn.com/avisos/1/00/44/82/57/21/360x266/1698490767.jpg"
+      }
     };
   }
+  
 
   formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
@@ -50,11 +61,28 @@ class App extends Component {
       showModal: false
     })
   }
-  handleOpenModal = e => {
+  handleOpenModal = (e) => {
     e.stopPropagation();
     this.setState({
       showModal: true
     })
+  }
+  galleryHandler = (e) =>{
+    let num = e.target.id;
+    let currentIndex = this.state.imagesIndex;
+    if(num==="s" && currentIndex<3)
+    {
+      currentIndex++
+    }
+    if(num==="m" && currentIndex>0)
+    {
+      currentIndex--
+    }
+    console.log(currentIndex)
+    this.setState({
+      imagesIndex: currentIndex
+    })
+
   }
   render() {
     let liked = null;
@@ -71,7 +99,18 @@ class App extends Component {
 
         <div className="container">
           <div className="multimedia">
-            <img src="https://imgar.zonapropcdn.com/avisos/1/00/44/82/57/21/360x266/1698490754.jpg" alt="" />
+            <img src={this.state.imagesSources[this.state.imagesIndex]} alt="" />
+            <span class="arrow-button left" type="button" onClick={this.galleryHandler} id="m">
+              <svg class="button-icon" viewBox="0 0 100 100" id="m">
+                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path>
+              </svg>
+            </span>
+            <span class="arrow-button right" type="button" onClick={this.galleryHandler} id="s">
+              <svg class="button-icon" viewBox="0 0 100 100" id="s">
+                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow" transform="translate(100, 100) rotate(180) " />
+              </svg>
+            </span>
+
             {liked}
             <div className="label">Super Destacado</div>
           </div>
